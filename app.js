586641774fbdfdd9c3880fa82b4196c36956f532
SamuelLK <samuel.losinskij-kovanko@hamk.fi>
2025-12-06 23:58:40 +0200

    Success

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed

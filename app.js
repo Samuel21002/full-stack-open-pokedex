640e980fdfa90170051358d18ae80e90621b03c6
SamuelLK <samuel.losinskij-kovanko@hamk.fi>
2025-12-06 23:41:30 +0200

    Simulate error_3

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('dist'))
+app.use(express.static('build'))
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 app.get('/health', (req, res) => {
-  res.status(500).send('failed')
-  throw new Error('fail health check') // uncomment to simulate a failed health check
+  res.send('ok')
 })
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
